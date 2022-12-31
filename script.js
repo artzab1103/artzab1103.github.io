4c8cbe1e9f9bb240b81a48a500e9dd3a217f22cf
artzab1103 <102410925+artzab1103@users.noreply.github.com>
2022-12-31 17:10:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $.getJSON(url, function(r) {
    return false;
  } 
 var pl = '';
- if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
+ if(r.players.sample.length > 0 ){ pl = '<br>Последний зашовший: '+r.players.sample[0].name;  } 
   $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Онлайн:</b> '+r.players.online+pl);
  $('#favicon').attr('src', r.favicon);
     
